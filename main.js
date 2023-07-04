c6a30a25b04dae1ed2a0dd1e68a91dc9c5992a45
Okoro Ikechi Emmanuel <87893545+KelzAce@users.noreply.github.com>
2023-07-04 16:51:06 +0100

    Rotating box

---
+++ main.js
@@ -0,0 +1,48 @@
+let scene, camera, renderer, cube;
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  //   const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+
+  const texture = new THREE.TextureLoader().load('textures/crate.gif');
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+}
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
